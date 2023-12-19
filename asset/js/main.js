eb1e440dee3a4eaff690863b932c19cd3a4039b0
Sufiyan6201 <sufiyanansari6201@gmail.com>
2023-12-19 14:36:31 +0530

    change That

--- asset/js/main.js
+++ asset/js/main.js
@@ -23,22 +23,57 @@ function singIn() {
 }
 // inquery
 function balnce() {
-  document.getElementById('next').style.display = 'none'
   document.getElementById('balnce').style.display = 'block'
   document.getElementById('depo').style.display = 'none'
+  document.getElementById('widrl').style.display = 'none'
+
 }
+
+
 // widrl
-function widrl() {
- var ac_number = 123456789
- var pass = 123456789
+function widrle() {
+  document.getElementById('widrl').style.display = 'block'
+  document.getElementById('balnce').style.display = 'none'
+  document.getElementById('depo').style.display = 'none'
+  document.getElementById('show1').value = blance;
+}
+
+
+function sub() {
+  var amount1 = document.getElementById('amount1').value
+  if (amount1 < blance && amount1 > 0) {
+    blance -= parseInt(amount1)
+    document.getElementById('show1').value = blance
+    alert('Withdrawal Succesfully Thank You For Using Our ATM')
+  } else {
+    alert('Pleas enter Valid Amount...')
+  }
 }
 // deposit
 function dep() {
-  document.getElementById('next').style.display = 'none'
-  document.getElementById('balnce').style.display = 'none'
-  document.getElementById('prev').style.display = 'none'
   document.getElementById('depo').style.display = 'block'
+  document.getElementById('balnce').style.display = 'none'
+  document.getElementById('widrl').style.display = 'none'
+  document.getElementById('show').value = blance;
 }
+
+
+function add() {
+  var amount = document.getElementById('amount').value
+  if (amount > 0) {
+    blance += parseInt(amount)
+    document.getElementById('show').value = blance;
+    alert('Deposit Succesfully Thank You For Using Our ATM')
+  } else {
+    alert('please enter valide amount')
+  }
+}
+var blance = 20000
+
+
+
+
+
 // exit
 function exit() {
   document.getElementById('next').style.display = 'none'
@@ -47,6 +82,7 @@ function exit() {
   document.getElementById('check').style.display = 'none'
 }
 
-function check(){
-  document.getElementById('check').style.display = 'block'
+function check() {
+  document.getElementById('check').innerHTML = '<span style="color:white;" >your balance:</span>&nbsp;&nbsp' + blance
+  return
 }
